c872047e3ed54bf7abccfdaf37d4f74b5ae33c43
Eduardo Valenzuela <e.valenzuelap@alumnos.duoc.cl>
2020-07-24 16:59:16 -0400

    Docs agregado

---
+++ docs/precache-manifest.fa0c373bccd9cf6b74e484f7bdb6997c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b12f5175ed722998c72538363e4d32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2b0aa353ba9d967d431",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8378a364590ef8de3f1",
+    "url": "/static/js/2.5722d8b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5722d8b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b0aa353ba9d967d431",
+    "url": "/static/js/main.8e3eddb7.chunk.js"
+  },
+  {
+    "revision": "734d1024b223b571db93",
+    "url": "/static/js/runtime-main.deb3e66d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
